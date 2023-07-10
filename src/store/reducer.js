bfaea6f0a868ced2eb3121f0dc66c71a1d81ad2b
smiadv2022 <msorokina@spacemedia.com.ua>
2023-07-10 23:38:24 +0300

    Update reducer.js

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,9 +2,10 @@ import { combineReducers } from 'redux';
 
 // import { modalReducer } from './Modal/modalSlice';
 // import { contactsReducer } from './Contacts/contactSlice';
-import { filterReducer } from './Filter/filterSlice';
+// import { filterReducer } from './Filter/filterSlice';
 import { modalReducer } from './Modal/modalSlice';
 import { contactsReducer } from './Contacts/contactSlice';
+import { filterReducer } from './Filter/filterSlice';
 
 export const reducer = combineReducers({
   contacts: contactsReducer,
